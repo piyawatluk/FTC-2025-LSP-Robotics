1439e2a54572f6975ade89ffb0051637f7fc4f03
piyawat luknatin <piyawatluknatin@gmail.com>
2025-08-12 23:17:14 +0700

    minor removal of static variable for gamepad selection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -11,9 +11,9 @@ public class MecanumDrive {
     }
 
     public void drive(Gamepad gamepad) {
-        double computedX_l = CoordinateConverter.computeX(gamepad1.left_stick_x, gamepad1.left_stick_y, 1);
-        double computedY_l = CoordinateConverter.computeY(gamepad1.left_stick_y, gamepad1.left_stick_y, 1);
-        double computedX_r = CoordinateConverter.computeX(gamepad1.right_stick_x, gamepad1.right_stick_y, 1);
+        double computedX_l = CoordinateConverter.computeX(gamepad.left_stick_x, gamepad1.left_stick_y, 1);
+        double computedY_l = CoordinateConverter.computeY(gamepad.left_stick_y, gamepad1.left_stick_y, 1);
+        double computedX_r = CoordinateConverter.computeX(gamepad.right_stick_x, gamepad1.right_stick_y, 1);
 
         double frontLeft = computedY_l + computedX_l + computedX_r;
         double frontRight = computedY_l - computedX_l - computedX_r;
