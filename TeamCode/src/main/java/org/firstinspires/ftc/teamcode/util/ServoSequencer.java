74830c9f5fac7795da1d904a3936e399e9f63bf4
itwinnert <itwinnerir@gmail.com>
2025-08-23 19:01:35 +0700

    interpolation is now optional

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ServoSequencer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ServoSequencer.java
@@ -11,13 +11,22 @@ public class ServoSequencer extends ServoUtil {
     ServoAction currentAction = null;
 
     public void addAction(Servo servo, double targetPos, long durationMs) {
-        actionQueue.add(new ServoAction(servo, targetPos, durationMs));
+        actionQueue.add(new ServoAction(servo, clamp(targetPos), durationMs));
+    }
+
+    public void addAction(Servo servo, double targetPos, long durationMs, boolean interpolate) {
+        actionQueue.add(new ServoAction(servo, clamp(targetPos), durationMs, interpolate));
     }
 
     // degree version of addAction
     public void addAction(Servo servo, int targetPos_deg, long durationMs, int maxDeg, int minDeg) {
         double targetPos = degToPos(targetPos_deg, maxDeg, minDeg);
-        addAction(servo, clamp(targetPos), durationMs);  // clamp makes sure the value isn't illegal
+        addAction(servo, clamp(targetPos), durationMs);
+    }
+
+    public void addAction(Servo servo, int targetPos_deg, long durationMs, int maxDeg, int minDeg, boolean interpolate) {
+        double targetPos = degToPos(targetPos_deg, maxDeg, minDeg);
+        addAction(servo, clamp(targetPos), durationMs, interpolate);
     }
 
     public void stepSequence() {
@@ -35,12 +44,19 @@ public class ServoSequencer extends ServoUtil {
             timer.reset();
         }
 
-        // interpolated movement
-        double t = clamp(timer.milliseconds() / currentAction.durationMs);
-        double interpolatedPos = currentAction.startPos + (currentAction.targetPos - currentAction.startPos) * t;
-        safeSetPosition(currentAction.servo, interpolatedPos);
-        if (t >= 1.0) {
-            currentAction = null; // Action is finished
+        if (currentAction.interpolate && currentAction.durationMs <= 0) {
+            // interpolated movement
+            double t = Math.min(1.0,timer.milliseconds() / currentAction.durationMs);
+            double interpolatedPos = currentAction.startPos + (currentAction.targetPos - currentAction.startPos) * t;
+            safeSetPosition(currentAction.servo, interpolatedPos);
+            if (t >= 1.0) {
+                currentAction = null; // Action is finished
+            }
+        } else {
+            safeSetPosition(currentAction.servo, currentAction.targetPos);
+            if (timer.milliseconds() >= currentAction.durationMs) {
+                currentAction = null;
+            }
         }
     }
 
@@ -54,15 +70,24 @@ public class ServoSequencer extends ServoUtil {
     }
 
     public static final class ServoAction {
-        public Servo servo;
-        public double targetPos;
-        public long durationMs;
+        public final Servo servo;
+        public final double targetPos;
+        public final long durationMs;
         public double startPos;
+        public final boolean interpolate;
 
         public ServoAction(Servo servo, double targetPos, long durationMs) {
             this.servo = servo;
             this.targetPos = targetPos;
             this.durationMs = durationMs;
+            this.interpolate = false;
+        }
+
+        public ServoAction(Servo servo, double targetPos, long durationMs, boolean interpolate) {
+            this.servo = servo;
+            this.targetPos = targetPos;
+            this.durationMs = durationMs;
+            this.interpolate = interpolate;
         }
     }
 }
