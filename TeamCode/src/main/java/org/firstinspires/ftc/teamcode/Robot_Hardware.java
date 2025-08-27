f69dbe2c06b15185b28c7bc9992834d34413817d
itwinnert <itwinnerir@gmail.com>
2025-08-27 15:06:26 +0700

    separated changes to servoUtil

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Hardware.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import java.io.InputStream;
@@ -18,17 +17,6 @@ public class Robot_Hardware {
     public DcMotor rearLeftMotor = null;
     public DcMotor frontRightMotor = null;
     public DcMotor rearRightMotor = null;
-
-    // Rename the servos
-    public String placeholderServoName1;
-    public String placeholderServoName2;
-    public String placeholderServoName3;
-    public String placeholderServoName4;
-
-    public Servo placeholderServo1 = null;
-    public Servo placeholderServo2 = null;
-    public Servo placeholderServo3 = null;
-    public Servo placeholderServo4 = null;
 
     public Robot_Hardware() {
         // Load config on object creation
@@ -48,12 +36,6 @@ public class Robot_Hardware {
         rearLeftMotorName = prop.getProperty("Robot.REAR_LEFT_MOTOR_NAME", "null");
         rearRightMotorName = prop.getProperty("Robot.REAR_RIGHT_MOTOR_NAME", "null");
 
-        /*
-        placeholderServoName1 = prop.getProperty("Robot.PLACEHOLDER_SERVO1_NAME", "null");
-        placeholderServoName2 = prop.getProperty("Robot.PLACEHOLDER_SERVO2_NAME", "null");
-        placeholderServoName3 = prop.getProperty("Robot.PLACEHOLDER_SERVO3_NAME", "null");
-        placeholderServoName4 = prop.getProperty("Robot.PLACEHOLDER_SERVO4_NAME", "null");
-        */
     }
 
     public void init(HardwareMap hardwareMap) {
@@ -72,16 +54,5 @@ public class Robot_Hardware {
         frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
         rearRightMotor.setDirection(DcMotor.Direction.FORWARD);
 
-        /*
-        placeholderServo1 = hardwareMap.get(Servo.class, placeholderServoName1);
-        placeholderServo2 = hardwareMap.get(Servo.class, placeholderServoName2);
-        placeholderServo3 = hardwareMap.get(Servo.class, placeholderServoName3);
-        placeholderServo4 = hardwareMap.get(Servo.class, placeholderServoName4);
-
-        placeholderServo1.setDirection(Servo.Direction.FORWARD);
-        placeholderServo2.setDirection(Servo.Direction.FORWARD);
-        placeholderServo3.setDirection(Servo.Direction.FORWARD);
-        placeholderServo4.setDirection(Servo.Direction.FORWARD);
-        */
     }
 }
