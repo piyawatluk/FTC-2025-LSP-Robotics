38c2bde25b2b01fab53fd628ee3af2bb1f468430
Crankyhotpotato <100062878+Watlaosin@users.noreply.github.com>
2025-09-04 10:10:39 +0700

    Update BackAndForthZiegler.java
    fixed syntax errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BackAndForthZiegler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BackAndForthZiegler.java
@@ -5,7 +5,10 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 // run this to figure out what is the ideal Ku and Pu then put in the Nichols-Ziegler urself in calculator or something
 @Config
@@ -13,6 +16,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 public class BackAndForthZiegler extends LinearOpMode {
 
     public static double kP_test = 0.0;
+    public static double TARGET_X = 24.0;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -27,33 +31,38 @@ public class BackAndForthZiegler extends LinearOpMode {
 
         double lastError = 0;
         double firstCrossingTime = -1;
-        double period = 0;
+        double Pu = 0;
 
         while (opModeIsActive() && !isStopRequested()) {
-            double error = drive.getPoseEstimate().getX();
-            double currentTime = getRuntime();
+
+            drive.setPIDFCoefficients(
+                    DcMotor.RunMode.RUN_USING_ENCODER,
+                    new PIDFCoefficients(kP_test, 0, 0, 0)
+                );
+
+            double currentX = drive.getPoseEstimate().getX();
+            double error = TARGET_X - currentX;
+            double now = getRuntime();
+
 
             if ((error > 0 && lastError < 0) || (error < 0 && lastError > 0)) {
-                if (firstCrossingTime < 0) {
-                    firstCrossingTime = currentTime;
-                } else {
-                    period = currentTime - firstCrossingTime;
-                    firstCrossingTime = currentTime;
+                if (lastCrossingTime > 0) {
+                    Pu = now - lastCrossingTime;
                 }
+                lastCrossingTime = now;
             }
-
             lastError = error;
 
             // Send data
             TelemetryPacket packet = new TelemetryPacket();
             packet.put("kP_test", kP_test);
             packet.put("Error", error);
-            packet.put("Oscillation Period", period);
+            packet.put("Pu (sec)", Pu);
             dashboard.sendTelemetryPacket(packet);
 
             telemetry.addData("Test kP", kP_test);
             telemetry.addData("Error", error);
-            telemetry.addData("Oscillation Period", period);
+            telemetry.addData("Pu (sec)", Pu);
             telemetry.update();
         }
     }
