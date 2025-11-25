ecdc0473dae14ad65d6c6cf006879914c06a719a
piyawat luknatin <piyawatluknatin@gmail.com>
2025-11-25 12:18:24 +0700

    max speed update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -73,7 +73,7 @@ public final class MecanumDrive {
         public double kA = 0.00055;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 50;
+        public double maxWheelVel = 160;
         public double minProfileAccel = -30;
         public double maxProfileAccel = 50;
 
